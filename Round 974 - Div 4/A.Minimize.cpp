8d92f7dc3ffc68724731c8fd39902b0aa2842ebe
varsha-0-3 <91776142+varsha-0-3@users.noreply.github.com>
2024-09-04 13:04:20 +0530

    Create A.Minimize.cpp

---
+++ Round 974 - Div 4/A.Minimize.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/2009/A
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t, a, b;
+    cin >> t;
+    while(t--){
+        cin >> a >> b;
+        cout << (b-a) << endl;
+    }
+    return 0;
+}
